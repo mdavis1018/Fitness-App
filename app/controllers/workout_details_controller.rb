c2d1ca04f5b7bfadd03f5e82ad9f12e0807bcfcb
Marcus Davis <marcusdavis1018@gmail.com>
2022-09-29 10:33:19 -0400

    category folder added

---
+++ app/controllers/workout_details_controller.rb
@@ -0,0 +1,7 @@
+class WorkoutDetailsController < ApplicationController
+  def create
+  WorkoutDetail.create(
+    
+  )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -24,4 +24,7 @@ Rails.application.routes.draw do
   get "/login" => "sessions#new"
   post "/sessions" => "sessions#create"
   get "/logout" => "sessions#destroy"
+
+  ## excercisecategory
+  resources :excercise_categories
 end
